0c17ea02033cd94b9b0ecfa6c7f90e11b5defabd
Keith Capers <K.Capers94@gmail.com>
2023-12-21 16:13:46 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+   return fetch("http://localhost:3000/users", {
+        method:"POST",
+        body: JSON.stringify({
+            name:name,
+            email: email
+        }),
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object);
+            document.querySelector("body").append(object.id)
+        })
+        .catch(function (error) {
+            alert("Bad things! Ragnarok!");
+            console.log(error.message);
+            document.querySelector("body").append(error.message)
+        });
+        
+}
+
